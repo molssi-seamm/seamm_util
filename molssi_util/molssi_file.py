ce22149322893bb06060a1a3454cf628c4431c8f
Paul Saxe <paul@thesaxes.net>
2018-12-20 15:33:37 -0500

    Starting to create output utilities

---
+++ molssi_util/molssi_file.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+"""A class for wrapping output from the MolSSI Framework and plugins.
+Initially it is a thin wrapper around a dictionary to store the output,
+using JSON to serialize and deserialize the data.
+
+It presents the dict api with the addition of methods to serialize and
+deserialize the contents.
+"""
+
+import bz2
+import collections.abc
+import gzip
+import json
+import logger
+import os.path
+import pprint
+
+
+class File():
+
+    def __init__(self, filename, mode,
+                 compression=None, organization='MolSSI',
+                 filetype=None, version=None):
+        self.filename = filename
+        self.mode = mode
+
+        # Depending on the mode, may require filetype and version
+        if self.mode == 'w':
+            if filetype is None:
+                raise ValueError("Filetype must be given for writable files!")
+            if version is None:
+                raise ValueError("Version must be given for writable files!")
+        elif self.mode == 'a':
+            if os.path.isfile(self.filename):
+                # Get the header and decipher
+                fd = self.__enter__()
+                try:
+                    line = next(fd)
+                except:
+                    raise Error("problem reading header line!")
+                
+                fd.close()
+
+                if line[0] == '!' and len(line.split()) == 3:
+                    organization, filetype, version = line[1:].split()
+            else:
+                logger.warning(
+                    "reading '{}', expected a header line but got\n\t'{}'".
+                    format(self.filename, line))
+
+                if filetype is None:
+                    raise ValueError(
+                        "Filetype must be given to create a file!")
+                if version is None:
+                    raise ValueError("Version must be given to create file!")
+
+        # Determine the type of the file
+        if compression is not None:
+            if compression not in ('text', 'bz2', 'gzip'):
+                raise ValueError("Invalid compression: '{}'"
+                                 .format(compression))
+        else:
+            extension = os.path.splitext(self.filename)[1].strip().lower()
+            if extension == '.bz2':
+                compression = 'bzip2'
+            elif extension == '.gz':
+                compression = 'gzip'
+            else:
+                compression = 'text'
+        self.compression = compression
+
+    def __enter__(self):
+        if self.compression == 'bzip2':
+            self.file_descriptor = bz2.open(self.filename, self.mode + 't')
+        elif self.compression == 'gzip':
+            self.file_descriptor = gzip.open(self.filename, self.mode + 't')
+        else:
+            self.file_descriptor = open(self.filename, self.mode)
+
+        return self.file_descriptor
+
+    def __exit__(self, *args):
+        self.file_descriptor.close()
+
+    def read_header(self):
+        if self.compression == 'bzip2':
+            with bz2.open(self.filename, 'rt') as fd:
+                line = next(fd)
+        elif self.compression == 'gzip':
+            with gzip.open(self.filename, 'rt') as fd:
+                line = next(fd)
+        else:
+            with open(self.filename, 'r'):
+                line = next(fd)
+
+        if line[0] == '!' and len(line.split()) == 3:
+            organization, filetype, version = line[1:].split()
+            logger.info(
+                "reading '{}', a {} file from {}, version {}".format(
+                    self.filename, filetype, organization, version))
+        else:
+            raise RuntimeError(
+                "reading '{}', expected a header line but got\n\t'{}'".
+                format(self.filename, line))
+        
+
---
+++ molssi_util/output.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+"""A class for wrapping output from the MolSSI Framework and plugins.
+Initially it is a thin wrapper around a dictionary to store the output,
+using JSON to serialize and deserialize the data.
+
+It presents the dict api with the addition of methods to serialize and
+deserialize the contents.
+"""
+
+import bz2
+import collections.abc
+import gzip
+import json
+import os.path
+import pprint
+
+
+class Output(collections.abc.MutableMapping):
+    def __init__(self,
+                 filename=None,
+                 **kwargs):
+
+        """Create the Output object
+
+        Keyword arguments:
+            filename: the name of the file for serialization
+            kwargs: any other keyword arguments initialize the dict
+        """
+
+        self._is_changed = False
+        self._data = dict()  # This stores the dict like data
+        self._filename = None
+
+        # Set the filename, if given, which will open the file
+        self.filename = filename
+
+        # Finally, process any extra keywords
+        if len(kwargs) > 0:
+            self.update(dict(**kwargs))
+
+    @property
+    def filename(self):
+        """The filename for the serialized data"""
+        return self._filename
+    
+    @filename.setter
+    def filename(self, filename):
+        if filename != self._filename:
+            if self._filename is None:
+                if self._is_changed:
+                    pass
+            else:
+                if self._is_changed:
+                    self.flush()
+            self._filename = filename
+
+        if self._filename is not None:
+            with self._open() as fd:
+                self._data = json.load(fd)
+            self._is_changed = False
+
+    def __getitem__(self, key):
+        """Allow [] access to the dictionary!"""
+        if key not in self._data:
+            raise KeyError("key '" + key + "' does not exist")
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        """Allow x[key] access to the data"""
+        self._data[key] = value
+        self._is_changed = True
+
+    def __delitem__(self, key):
+        """Allow deletion of keys"""
+        del self._data[key]
+        self._is_changed = True
+
+    def __iter__(self):
+        """Allow iteration over the object"""
+        return iter(self._data)
+
+    def __len__(self):
+        """The len() command"""
+        return len(self._data)
+
+    def __repr__(self):
+        """The string representation of this object"""
+        return repr(self._data)
+
+    def __str__(self):
+        """The pretty string representation of this object"""
+        return pprint.pformat(self._data)
+
+    def copy(self):
+        """Return a shallow copy of the dictionary"""
+        return self._data.copy()
+    
+    def _open(self):
+        """Open self.filename, using compression according to its extension,
+        """
+
+        extension = os.path.splitext(self.filename)[1].strip().lower()
+        if extension == '.bz2':
+            fd = bz2.open(self.filename, 'rt')
+        elif extension == '.gz':
+            fd = gzip.open(self.filename, 'rt')
+        else:
+            fd = open(self.filename, 'r')
+
+        return fd
+
