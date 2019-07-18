b7e8e012eab71ce1e17077b124f2769dc08eb750
Paul Saxe <psaxe@thesaxes.net>
2019-07-18 09:32:01 -0400

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='seamm_util',
-    version='0.1.0',
+    version='0.2.0',
     description="Utility classes and functions",
     long_description=readme + '\n\n' + history,
     author="Paul Saxe",
