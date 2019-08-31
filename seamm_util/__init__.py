e25e73522ea095eb5f184e68b204a486edabfac1
Paul Saxe <paul@thesaxes.net>
2019-08-31 17:41:59 -0400

    Switched to versioneer for versioning.

--- seamm_util/__init__.py
+++ seamm_util/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for SEAMM Util."""
+"""
+seamm_util
+Utility functions for the SEAMM environment.
+"""
 
-__author__ = """Paul Saxe"""
-__email__ = 'psaxe@molssi.org'
-__version__ = '0.2.1'
+# Bring up the classes so that they appear to be directly in
+# the packmol_step package.
 
 from seamm_util.units import ureg, Q_, units_class  # noqa: F401
 from seamm_util.include_open import Open  # noqa: F401
@@ -15,3 +17,12 @@ import seamm_util.molfile  # noqa: F401
 import seamm_util.printing  # noqa: F401
 # import seamm_util.md_statistics  # noqa: F401
 import seamm_util.variable_names  # noqa: F401
+
+# Handle versioneer
+from ._version import get_versions
+__author__ = """Paul Saxe"""
+__email__ = 'psaxe@molssi.org'
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
