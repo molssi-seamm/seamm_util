8bd0c610e6489c96b06b2dcf2cb3fe659dcfead6
Paul Saxe <paul@thesaxes.net>
2021-06-06 13:49:44 -0400

    Added `items` method needed for Interpolation.

--- seamm_util/argument_parser.py
+++ seamm_util/argument_parser.py
@@ -438,6 +438,43 @@ class ArgumentParser(object):
                         parser.add_argument(*args, **kwargs)
                 return parser.format_help()
 
+    def items(self, section=_UNSET, raw=False, vars=None):
+        """Return a list of (name, value) tuples for each option in a section.
+
+        All % interpolations are expanded in the return values, based on the
+        defaults passed into the constructor, unless the optional argument
+        `raw' is true.  Additional substitutions may be provided using the
+        `vars' argument, which must be a dictionary whose contents overrides
+        any pre-existing defaults.
+
+        The section DEFAULT is special.
+        """
+        if section is _UNSET:
+            return super().items()
+        try:
+            d = self._options[section]
+        except KeyError:
+            if section != self.default_section:
+                raise configparser.NoSectionError(section)
+        orig_keys = list(d.keys())
+        # Update with the entry specific variables
+        if vars:
+            for key, value in vars.items():
+                d[self.optionxform(key)] = value
+        if raw:
+
+            def value_getter(option):
+                return d[option]
+
+        else:
+
+            def value_getter(option):
+                return self._interpolation.before_get(
+                    self, section, option, d[option], d
+                )
+
+        return [(option, value_getter(option)) for option in orig_keys]
+
     def split_args(self, args=None):
         """Break the argument line into sections for each plug-in.
 
